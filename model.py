aed0816269b38542b722a048f58a85708b4beae1
Arun Joshi <jarun6069@gmail.com>
2024-10-08 08:02:23 +0545

    Tracked initial Files

---
+++ model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 07:46:16 2024
+
+@author: Arun Joshi
+"""
+
---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 07:44:09 2024
+
+@author: LENOVO
+"""
+
+import sys
+
+print(sys.executable)
